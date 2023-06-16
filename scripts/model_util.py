ba42797056e3ee3dd1cc9ca2738bee8d4d9d9e99
Jackstrawcd <35131303+Jackstrawcd@users.noreply.github.com>
2023-06-16 19:17:20 +0800

    Update model_util.py

--- scripts/model_util.py
+++ scripts/model_util.py
@@ -8,7 +8,7 @@ import tqdm
 from collections import OrderedDict
 from multiprocessing.pool import ThreadPool as Pool
 
-from modules import shared, sd_models, hashes
+from modules import shared, sd_models, hashes, paths
 from scripts import safetensors_hack, model_util, util
 import modules.scripts as scripts
 
@@ -20,8 +20,7 @@ re_legacy_hash = re.compile("\(([0-9a-f]{8})\)$")  # matches 8-character hashes,
 lora_models = {}  # "My_Lora(abcdef123456)" -> "C:/path/to/model.safetensors"
 lora_model_names = {}  # "my_lora" -> "My_Lora(My_Lora(abcdef123456)"
 legacy_model_names = {}
-# lora_models_dir = os.path.join(scripts.basedir(), "models/lora")
-lora_models_dir = "user-models/Lora"
+lora_models_dir = paths.user_models_path
 os.makedirs(lora_models_dir, exist_ok=True)
 
 
